a7328db3f65400da321516c2fd922c20361ff121
allennssce <allen.nssce@gmail.com>
2019-07-05 02:20:13 +0530

    Content Work 05-07-2019 02:20 AM

--- controllers/icrtseController.js
+++ controllers/icrtseController.js
@@ -1,5 +1,29 @@
-module.exports = function (app){
+var fs = require('fs')
+var mongoose = require('mongoose');
+
+var rawdata = fs.readFileSync('json/palakkad.json');  
+var palakkad = JSON.parse(rawdata);  
+var rawdata = fs.readFileSync('json/notifications.json');
+var notification = JSON.parse(rawdata);
+console.log(notification);  
+
+module.exports = function (app){    
+
     app.get('/icrtse2020', function(req, res){
-        res.render('home');
+        res.render('home', {palakkad : palakkad, notification : notification});
     });
+
+    app.get('/home', function(req, res){
+        res.redirect('icrtse2020')
+    });
+
+    app.get('/index', function(req, res){
+        res.redirect('icrtse2020')
+    });
+
+    app.get('*', function(req, res){
+        res.render('404', {palakkad : palakkad, notification : notification});
+    });
+
+    
 }
