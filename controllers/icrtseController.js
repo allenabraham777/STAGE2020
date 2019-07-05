b166b41ac7003ffe5996f216096570e439c31991
allennssce <allen.nssce@gmail.com>
2019-07-06 00:51:30 +0530

    Tweaks 06-07-2019

--- controllers/icrtseController.js
+++ controllers/icrtseController.js
@@ -39,7 +39,7 @@ module.exports = function (app){
         res.render('construction');
     });
     app.get('/contact', function(req, res){
-        res.render('construction');
+        res.render('contact');
     });
     
 
