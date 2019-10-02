bc4a989a4b45752b88a811f7798895fd8ae82eb4
allenabraham777 <allenabraham777@gmail.com>
2019-10-02 23:25:40 +0530

    Keynote and IC

--- controllers/icrtseController.js
+++ controllers/icrtseController.js
@@ -1,12 +1,12 @@
 var fs = require('fs')
 var mongoose = require('mongoose');
 
-var rawdata = fs.readFileSync('json/palakkad.json');  
-var palakkad = JSON.parse(rawdata);  
+var rawdata = fs.readFileSync('json/palakkad.json');
+var palakkad = JSON.parse(rawdata);
 var rawdata = fs.readFileSync('json/notifications.json');
 var notification = JSON.parse(rawdata);
 
-module.exports = function (app){    
+module.exports = function (app){
 
     app.get('/icrtse2020', function(req, res){
         res.render('home', {palakkad : palakkad, notification : notification});
@@ -34,6 +34,9 @@ module.exports = function (app){
     app.get('/schedule', function(req, res){
         res.render('schedule');
     });
+    app.get('/keynote', function(req, res){
+        res.render('keynote');
+    });
 
     app.get('/call_for_paper', function(req, res){
         res.render('construction');
@@ -41,10 +44,10 @@ module.exports = function (app){
     app.get('/contact', function(req, res){
         res.render('contact');
     });
-    
+
 
     app.get('*', function(req, res){
         res.render('404', {palakkad : palakkad, notification : notification});
     });
-    
+
 }
