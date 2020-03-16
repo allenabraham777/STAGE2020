d8eb336d6824df09cba90d39e2b3bd1dbcd68191
Allen K Abraham <allenabraham777@gmail.com>
2020-03-16 12:21:21 +0530

    Changes made at the time of Corona

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var icrtseContrller = require('./controllers/stageController')
 var app = express();
 
 // sets port 8080 to default or unless otherwise specified in the environment
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 
 //Set up template Engine
 app.set('view engine', 'ejs');
